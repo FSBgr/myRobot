23465d7b3f153350b83488ada1539cb5430f6223
Chris <christpc@csd.auth.gr>
2024-01-16 00:28:09 +0200

    world building final

--- src/com/company/Env.java
+++ src/com/company/Env.java
@@ -1,30 +1,41 @@
-// Env.java
 package com.company;
 import javax.vecmath.Color3f;
 import javax.vecmath.Vector3d;
 import javax.vecmath.Vector3f;
-
 import simbad.sim.*;
 
 import java.awt.*;
 
 public class Env extends EnvironmentDescription {
     public Color3f white = new Color3f(Color.white);
-    public Color3f ligthgray = new Color3f(Color.lightGray);
-    /*public Color3f ambientLightColor = new Color3f(Color);*/
-   /*public Color3f gray;
-    public Color3f darkgray;
-    public Color3f black;
-    public Color3f red;
-    public Color3f green;
-    public Color3f blue;*/
+    public Color3f lightGray = new Color3f(Color.lightGray);
+    public Color3f yellow = new Color3f(Color.yellow);
+    public Color3f magenta = new Color3f(Color.MAGENTA);
+
+    public Wall wall1, wall2, wall3, wall4;
 
     Env(){
+        wall1 = new Wall(new Vector3d(5,0,4), 4,1, this);
+        wall2 = new Wall(new Vector3d(3,0,2.1), 4,1, this);
+        wall2.rotate90(1);
+        wall3 = new Wall(new Vector3d(0,0,-5), 5, 4, this);
+        wall3.rotate90(1);
+        wall3.setColor(yellow);
+        wall4 = new Wall(new Vector3d(0,0,7), 4,1, this);
+        wall4.setColor(magenta);
+        wall4.rotateY(-0.5);
+        wall4.rotate90(1);
+
         add(new CherryAgent(new Vector3d(7,0,7),"cherry",0.1f));
         add(new MyRobot(new Vector3d(-7, 0, -7), "robot 1"));
-        add(new Wall(new Vector3d(5,0,4), 3, 1, this));
-        add(new Arch(new Vector3d(-3,0,-5), this));
+        add(new Arch(new Vector3d(-5,0,-5), this));
         add(new Box(new Vector3d(-3,0,-2), new Vector3f(1,1,1),this));
+        add(new Wall(new Vector3d(-5, 0, 5), 4,2, this));
+        add(wall1);
+        add(wall2);
+        add(wall3);
+        add(wall4);
+
         light1SetPosition(7, 2, 7);
         light1IsOn = false;
         light2SetPosition(7, 2, 7);
@@ -32,7 +43,7 @@ public class Env extends EnvironmentDescription {
         setWorldSize(20);
         showAxis(true);
         ambientLightColor = white;
-        backgroundColor = ligthgray;
+        backgroundColor = lightGray;
 
     }
 }
