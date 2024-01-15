ae853769caf835488f88f79f216ab3abec992b03
Chris <christpc@csd.auth.gr>
2024-01-16 01:00:00 +0200

    world building adjustment and light sensors added

--- src/com/company/MyRobot.java
+++ src/com/company/MyRobot.java
@@ -5,10 +5,15 @@ import simbad.sim.LightSensor;
 import simbad.sim.RobotFactory;
 
 public class MyRobot extends Agent {
-    LightSensor light;
+    LightSensor left, right, middle ;
     public MyRobot (Vector3d position, String name) {
         super(position,name);
-        light = RobotFactory.addLightSensor(this);
+        middle = RobotFactory.addLightSensor(this);
+        left = RobotFactory.addLightSensor(this, new Vector3d(0.18, 0.25, 0.18), 1, "left");
+        right = RobotFactory.addLightSensor(this, new Vector3d(0.18, 0.25, -0.18), 1, "right");
+        //left = RobotFactory.addLightSensorLeft(this);
+        //right = RobotFactory.addLightSensorRight(this);
+
     }
     public void initBehavior() {
         setTranslationalVelocity(0.5);
