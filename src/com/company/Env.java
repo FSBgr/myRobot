a20bed3001457bc40b386a55b65d651633529f15
Chris <christpc@csd.auth.gr>
2024-01-16 23:46:23 +0200

    some worthless code

--- src/com/company/Env.java
+++ src/com/company/Env.java
@@ -13,6 +13,7 @@ public class Env extends EnvironmentDescription {
 
     Env(){
         setWorldSize(100);
+
         wall1 = new Wall(new Vector3d(1,0,4), 4,1, this);
         wall2 = new Wall(new Vector3d(3,0,5.5), 3,1, this);
         wall2.rotate90(1);
@@ -20,17 +21,18 @@ public class Env extends EnvironmentDescription {
         wall3.rotate90(1);
         wall4 = new Wall(new Vector3d(1, 0, 7), 4,1, this);
 
-        add(new CherryAgent(new Vector3d(7,0,7),"cherry",0.1f));
-        add(new MyRobot(new Vector3d(-7, 0, -7), "robot 1"));
-        add(new Arch(new Vector3d(-5,0,-5), this));
+        add(new Box(new Vector3d(-5,0,-5), new Vector3f(1,1,1), this));
         add(new Box(new Vector3d(-3,0,-2), new Vector3f(1,1,1),this));
         add(new Box(new Vector3d(-4,0,1), new Vector3f(1,1,1),this));
         add(new Box(new Vector3d(-5,0,4), new Vector3f(1,1,1),this));
         add(new Box(new Vector3d(-4,0,7), new Vector3f(1,1,1),this));
-        add(wall1);
+        /*add(wall1);
         add(wall2);
         add(wall3);
-        add(wall4);
+        add(wall4);*/
+
+        add(new CherryAgent(new Vector3d(7,0,7),"cherry",0.1f));
+        add(new MyRobot(new Vector3d(-7, 0, -7), "robot 1"));
 
         light1SetPosition(-7, -6, -7);
         light1Color = new Color3f(Color.BLACK);
